c58f0ef4d14bbd3d8219e79aaca94c1682944a79
Harut2003 <harutyun.t2.y@tumo.org>
2019-12-27 20:06:27 +0400

    adding about cards

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,74 +1,80 @@
 <template>
   <div>
-    <v-parallax
-    dark
-    height="500"
-    src="https://cdn.hipwallpaper.com/i/29/62/ISWR7o.jpg"
-  >
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col class="text-center" cols="12">
-        <h1 class="display-1 font-weight-thin mb-4 black--text">Armenian Events</h1>
-        <h4 class="subheading black--text">About us</h4>
+    <v-parallax dark height="500" src="https://cdn.hipwallpaper.com/i/29/62/ISWR7o.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="display-1 font-weight-thin mb-4 black--text">Armenian Events</h1>
+          <h4 class="subheading black--text">About us</h4>
+        </v-col>
+      </v-row>
+    </v-parallax>
+    <h2 class="mt-12 mb-6 display-2">Meet our team</h2>
+    <v-row align="center" justify="center">
+      <v-col cols="4">
+        <v-hover v-slot:default="{ hover }">
+          <v-card :elevation="hover ? 12 : 2" class="mx-auto" max-width="350">
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title>Harut Sargsyan</v-card-title>
+            </v-img>
+
+            <v-card-subtitle class="pb-0">Front-end developer</v-card-subtitle>
+
+            <v-card-text class="text--primary">
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae temporibus repellendus. Saepe iusto aspernatur ipsa aliquid illum. Neque libero doloribus nemo facilis quis delectus cumque, sapiente vel corrupti quidem.</div>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-col>
+      <v-col cols="4">
+        <v-hover v-slot:default="{ hover }">
+          <v-card :elevation="hover ? 12 : 2" class="mx-auto" max-width="350">
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title>Arman Khandanyan</v-card-title>
+            </v-img>
+
+            <v-card-subtitle class="pb-0">Back-end developer</v-card-subtitle>
+
+            <v-card-text class="text--primary">
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est tenetur possimus impedit. Doloribus cumque suscipit, tenetur tempore expedita sapiente cupiditate nostrum accusamus laborum dolores, maxime quas fugit! Neque, ratione veritatis?</div>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-col>
+      <v-col cols="4">
+        <v-hover v-slot:default="{ hover }">
+          <v-card :elevation="hover ? 12 : 2" class="mx-auto" max-width="350">
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title>Karine Ter-Minasyan</v-card-title>
+            </v-img>
+
+            <v-card-subtitle class="pb-0">Front-end developer</v-card-subtitle>
+
+            <v-card-text class="text--primary">
+              <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil ipsum architecto exercitationem quisquam commodi quis quasi perspiciatis amet voluptatum unde maxime incidunt, eum inventore voluptatibus aut repellendus nemo deserunt modi!</div>
+            </v-card-text>
+          </v-card>
+        </v-hover>
       </v-col>
     </v-row>
-  </v-parallax>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
-  <h2 class="mt-12 mb-6 display-2">Meet our team</h2>
-  <v-row
-    align="center"
-    justify="center"
-  >
-    <v-col cols="4">
-      <v-hover v-slot:default="{ hover }">
-        <v-card
-          :elevation="hover ? 12 : 2"
-          class="mx-auto"
-          height="350"
-          max-width="350"
-        >
-          <v-card-text class="my-4 text-center title">Harut</v-card-text>
-        </v-card>
-      </v-hover>
-    </v-col>
-    <v-col cols="4">
-      <v-hover v-slot:default="{ hover }">
-        <v-card
-          :elevation="hover ? 12 : 2"
-          class="mx-auto"
-          height="350"
-          max-width="350"
-        >
-          <v-card-text class="my-4 text-center title">Arman</v-card-text>
-        </v-card>
-      </v-hover>
-    </v-col>
-    <v-col cols="4">
-      <v-hover v-slot:default="{ hover }">
-        <v-card
-          :elevation="hover ? 12 : 2"
-          class="mx-auto"
-          height="350"
-          max-width="350"
-        >
-          <v-card-text class="my-4 text-center title">Karine</v-card-text>
-        </v-card>
-      </v-hover>
-    </v-col>
-  </v-row>
   </div>
 </template>
  
  
 <script>
 // @ is an alias to /src
- 
+
 export default {
   name: "About",
   components: {}
